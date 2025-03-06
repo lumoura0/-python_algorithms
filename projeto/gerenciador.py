493b0d0facf9e254a9087cd5a862457eda7d2eb2
Luis Fernando <luisfernando.mourag@gmail.com>
2025-03-06 10:40:36 -0300

    Finalizando projeto de tarefas

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -5,6 +5,9 @@ def adicionar_tarefa(tarefas, nome_tarefa=""):
     return
 
 def ver_tarefas(tarefas):
+    if not tarefas:
+        print("Nenhuma tarefa cadastrada!")
+        return
     for indice, tarefa in enumerate(tarefas, start=1):
         status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa["tarefa"]
@@ -25,6 +28,13 @@ def completar_tarefa(tarefas, indice_tarefa):
     indice_tarefa_ajustado = indice_tarefa -1
     tarefas[indice_tarefa_ajustado]["completada"] = True
     print(f"Tarefa {indice_tarefa} marcada como completada!")
+    return
+
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print("Tarefas completadas foram deletadas!")
     return
 
 tarefas = []
@@ -54,6 +64,9 @@ while True:
         ver_tarefas(tarefas)
         indice_tarefa = int(input("Digite o indice da tarefa que deseja completar: "))
         completar_tarefa(tarefas, indice_tarefa)
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
 
